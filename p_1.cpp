d36bcd746faa67f4e5da8c768507f7ac341a4c45
RianIslam <rianislamdr@gmail.com>
2021-01-29 01:33:10 +0600

    learning basic

--- p_1.cpp
+++ p_1.cpp
@@ -7,12 +7,15 @@ int main()
 
     double base,height,area;
 
-    cout <<"ENTER BASE :";
+    cout <<"ENTER base :";
     cin >> base;
 
-    cout << "ENTER BASE :";
-    cin >> base;
+    cout << "ENTER Height :";
+    cin >> height;
 
+    area = (double)1/2 * base * height;
+
+    cout << "area of triangle = " << area;
 
 getch();
 }
