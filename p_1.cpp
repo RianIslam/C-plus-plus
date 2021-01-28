51d3bd4a125286f979f365f831b7ee02156cdb6b
RianIslam <rianislamdr@gmail.com>
2021-01-29 01:16:59 +0600

    c++ practice

---
+++ p_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+    double base,height,area;
+
+    cout <<"ENTER BASE :";
+    cin >> base;
+
+    cout << "ENTER BASE :";
+    cin >> base;
+
+
+getch();
+}
