e119b096d6361a2145aa66cf33c9570fc6d64321
RianIslam <rianislamdr@gmail.com>
2021-01-31 21:23:09 +0600

    array for

---
+++ array_for.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+
+int marks[7] = {443,334,43,533,56,78,44};
+
+for(int i= 0; i<7; i++)
+{
+    cout << marks[i] << " ";
+}
+
+
+getch();
+
+}
