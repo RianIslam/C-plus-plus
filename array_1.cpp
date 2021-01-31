aaf8dbb5428417f2598969dea9cee73465350379
RianIslam <rianislamdr@gmail.com>
2021-01-31 21:05:01 +0600

    array

---
+++ array_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+
+int marks[5]= {39,39,191,30,30};
+
+cout << marks[0] << " ";
+cout << marks[1] << " ";
+cout << marks[2] << " ";
+cout << marks[3] << " ";
+cout << marks[4] << " ";
+
+getch();
+}
