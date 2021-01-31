8d705ea188d4e74a5eff35cd51be23c358388be6
RianIslam <rianislamdr@gmail.com>
2021-01-31 21:47:06 +0600

    add input

---
+++ array_1_input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+
+int marks[5];
+for(int i = 0; i<5; i++){
+    cout << "Marks for students " << i+1 << "=";
+    cin >> marks[i];
+}
+cout << "Marks are : ";
+for(int i=0; i<5;i++){
+    cout << marks[i] << " ";
+}
+
+}
