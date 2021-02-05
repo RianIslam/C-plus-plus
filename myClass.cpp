fbb3949c993cafa478a8a637751ddca551ea7a98
RianIslam <rianislamdr@gmail.com>
2021-02-05 19:28:47 +0600

    class add

--- myClass.cpp
+++ myClass.cpp
@@ -3,13 +3,24 @@
 using namespace std;
 
 class Student{
+    public :
 int id;
 double gpa;
-
 };
 
 int main(){
 
+Student Alim;
+Alim.id = 199;
+Alim.gpa = 3.33;
+
+cout << Alim.id<< "  " << Alim.gpa <<endl;
+
+Student halu;
+halu.id = 199;
+halu.gpa = 3.33;
+
+cout << halu.id << "  " << halu.gpa;
 getch();
 }
 
