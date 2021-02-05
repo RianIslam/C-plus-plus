0dcd24a3ad85e443a86a3bbd0b66b62f78ea2768
RianIslam <rianislamdr@gmail.com>
2021-02-05 20:28:17 +0600

    void function

--- myClassVoid.cpp
+++ myClassVoid.cpp
@@ -3,12 +3,19 @@
 using namespace std;
 
 class Student {
-public
-int id:
-double gpa:
+public:
+int id;
+double gpa;
+void display(){
+cout << id << " " << gpa<< endl;
+}
 
 };
 
 int main(){
+    Student alim,hero;
+    alim.id = 333;
+    alim.gpa = 3.345;
+    alim.display();
 getch();
 }
